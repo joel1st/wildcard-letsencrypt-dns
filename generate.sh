006ad23d5e7b7a6cd7fbfa5c5397e4f1165ceda8
Joel Olsson <joel_1st@hotmail.com>
2018-03-24 00:41:29 +1000

    rm container after use

--- generate.sh
+++ generate.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 rm -rf ssl_out
 
-docker run -it --name certbot -v "`echo ~/.aws`:/root/.aws" -v "`pwd`/ssl_out:/etc/letsencrypt/archive/" certbot/dns-route53 certonly -n --email $EMAIL --agree-tos --preferred-challenges dns --server https://acme-v02.api.letsencrypt.org/directory --dns-route53 --dns-route53-propagation-seconds 60 -d $DOMAIN -d "*.$DOMAIN"
+docker run -it --rm --name certbot -v "`echo ~/.aws`:/root/.aws" -v "`pwd`/ssl_out:/etc/letsencrypt/archive/" certbot/dns-route53 certonly -n --email $EMAIL --agree-tos --preferred-challenges dns --server https://acme-v02.api.letsencrypt.org/directory --dns-route53 --dns-route53-propagation-seconds 60 -d $DOMAIN -d "*.$DOMAIN"
 
 if [[ $BUCKET ]]; then
   aws s3 mb s3://$BUCKET
