7d9cd2f07713f26a14291e3858f0cc84e4618834
Carl Foster <carl.foster@abx.com>
2018-04-19 16:08:23 +1000

    Enable aws profile passthrough

--- generate.sh
+++ generate.sh
@@ -1,7 +1,16 @@
 #!/bin/bash
 rm -rf ssl_out
 
-docker run -it --rm --name certbot -v "`echo ~/.aws`:/root/.aws" -v "`pwd`/ssl_out:/etc/letsencrypt/archive/" certbot/dns-route53 certonly -n --email $EMAIL --agree-tos --preferred-challenges dns --server https://acme-v02.api.letsencrypt.org/directory --dns-route53 --dns-route53-propagation-seconds 60 -d $DOMAIN -d "*.$DOMAIN"
+docker run -it --rm --name certbot \
+  -v "`echo ~/.aws`:/root/.aws" \
+  -v "`pwd`/ssl_out:/etc/letsencrypt/archive/" \
+  --env AWS_PROFILE=$AWS_PROFILE \
+  certbot/dns-route53 certonly \
+  -n --email $EMAIL \
+  --agree-tos --preferred-challenges dns \
+  --server https://acme-v02.api.letsencrypt.org/directory \
+  --dns-route53 --dns-route53-propagation-seconds 60 \
+  -d $DOMAIN -d "*.$DOMAIN"
 
 if [[ $BUCKET ]]; then
   aws s3 mb s3://$BUCKET
