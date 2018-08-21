f72b5289f653c7bd20400a9e164eaa0a1ef3245b
joel1st <joel_1st@hotmail.com>
2018-08-21 12:59:47 +1000

    Update generate.sh

--- generate.sh
+++ generate.sh
@@ -13,6 +13,13 @@ docker run -it --rm --name certbot \
   --dns-route53 --dns-route53-propagation-seconds 60 \
   -d $DOMAIN -d "*.$DOMAIN"
 
+cd ssl_out/$DOMAIN/
+mv fullchain1.pem fullchain.pem
+mv privkey1.pem privkey.pem
+mv cert1.pem cert.pem
+mv chain1.pem chain.pem
+cd ../../
+
 if [[ $BUCKET ]]; then
   aws s3 mb s3://$BUCKET
   aws s3 sync ssl_out s3://$BUCKET/
